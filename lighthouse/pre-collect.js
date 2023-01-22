cf4c2daf1e0cf721a64d1fd071089564aa360107
mariia.sampir <marysaphpir@gmail.com>
2023-01-22 11:38:18 +0200

    assertion - 7

--- lighthouse/pre-collect.js
+++ lighthouse/pre-collect.js
@@ -1,38 +1,29 @@
+// puppeteer-extra is a drop-in replacement for puppeteer,
+// it augments the installed puppeteer with plugin functionality
 const puppeteer = require('puppeteer-extra')
+
+// add recaptcha plugin and provide it your 2captcha token
+// 2captcha is the builtin solution provider but others work as well.
 const RecaptchaPlugin = require('puppeteer-extra-plugin-recaptcha')
-
-module.exports = async browser => {
-  const page = await browser.newPage();
-
-  page.setCookie({
-    domain: 'https://independer.nl',
-    name: 'set-cookie',
-    value:
-      'BIGipServer~development~pool_master.independer.nl_DefaultWebsitePool=!sSeyaP3rMK39bW9c8aPI61iczy6Eaf1KmhCh/OroPJ4UUN3DTqYPDTwgmdEOZQ1ftR4HWJH9JBwZWC9iTm9lIkXNNh7ALOCqGhyIvIXNe4U=; expires=Fri, 20-Jan-2023 15:31:59 GMT; path=/; Httponly; Secure',
-  });
-
-  puppeteer.use(
-    RecaptchaPlugin({
-      provider: { id: '2captcha', token: 'XXXXXXX' },
-      visualFeedback: true // colorize reCAPTCHAs (violet = detected, green = solved)
-    })
-  )
-
-  // puppeteer usage as normal
-  puppeteer.launch({ headless: true }).then(async browser => {
-    const page = await browser.newPage()
-    await page.goto('https://www.independer.nl/')
-
-    // That's it, a single line of code to solve reCAPTCHAs 🎉
-    await page.solveRecaptchas()
-
-    await Promise.all([
-      page.waitForNavigation(),
-      page.click(`#recaptcha-demo-submit`)
-    ])
-    await page.screenshot({ path: 'response.png', fullPage: true })
-    await browser.close()
+puppeteer.use(
+  RecaptchaPlugin({
+    provider: { id: '2captcha', token: 'XXXXXXX' },
+    visualFeedback: true // colorize reCAPTCHAs (violet = detected, green = solved)
   })
+)
 
-  page.solveRecaptchas()
-};
+// puppeteer usage as normal
+puppeteer.launch({ headless: true }).then(async browser => {
+  const page = await browser.newPage()
+  await page.goto('https://www.independer.nl/')
+
+  // That's it, a single line of code to solve reCAPTCHAs 🎉
+  await page.solveRecaptchas()
+
+  await Promise.all([
+    page.waitForNavigation(),
+    page.click(`#recaptcha-demo-submit`)
+  ])
+  await page.screenshot({ path: 'response.png', fullPage: true })
+  await browser.close()
+})
