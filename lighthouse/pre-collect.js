b0fb859eb5da05f182b38c3a31e034aaf75eb538
mariia.sampir <marysaphpir@gmail.com>
2023-01-22 11:30:39 +0200

    assertion - 4

--- lighthouse/pre-collect.js
+++ lighthouse/pre-collect.js
@@ -2,7 +2,7 @@ module.exports = async browser => {
   const page = await browser.newPage();
 
   page.setCookie({
-    domain: 'https://master.independer.nl/api/security/loggedonuser',
+    domain: 'https://independer.nl',
     name: 'set-cookie',
     value:
       'BIGipServer~development~pool_master.independer.nl_DefaultWebsitePool=!sSeyaP3rMK39bW9c8aPI61iczy6Eaf1KmhCh/OroPJ4UUN3DTqYPDTwgmdEOZQ1ftR4HWJH9JBwZWC9iTm9lIkXNNh7ALOCqGhyIvIXNe4U=; expires=Fri, 20-Jan-2023 15:31:59 GMT; path=/; Httponly; Secure',
